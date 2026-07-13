754344603884663952c6ed94d6a75052394e6e92
André Pedro <apedro.1@gmail.com>
2026-07-14 00:10:32 +0100

    Add problem_id to submission type and update related functions; enhance submissions list component to fetch problem details.

--- src/components/submissions_list.ml
+++ src/components/submissions_list.ml
@@ -73,42 +73,47 @@ let content ~contest_id () =
       in
       Api.Helpers.fetch_json url
       >>= fun (resp, status) ->
-      
       if status <> 200 then (
         Console.console##log
           (Js.string
-             (Printf.sprintf "Failed to fetch submissions: %d" status)) ;
+             (Printf.sprintf "Failed to fetch submissions: %d" status) ) ;
         Lwt.return_unit )
-      else
-        (* code 200 *)
-      let resp_json_string = Js.to_string (Json.output resp) in
-      let submissions : Api.Openapi.submission list =
-        Api.Openapi.ContestsContestidSubmissionsGetResponse2.of_json
-          resp_json_string
-      in
-      List.iter
-        (fun (sub : Api.Openapi.submission) ->
-          let id = sub.id in
-          let status = status_of_string sub.status in
-          let time_str =
-            format_time_ms sub.time_ms
-          in
-          (*let problem =
-            match Js.Opt.get sub.problem_id (fun () -> Js.string "?") with
-            | s -> Js.to_string s
-          in
-          let lang =
-            match
-              Js.Opt.get sub.language (fun () -> Js.string "Unknown")
-            with
-            | s -> Js.to_string s
-          in*)
-          let problem = "" in
-          let lang = "Unknown" in
-          let row = submission_row id problem lang status time_str in
-          Dom.appendChild
-            (Tyxml_js.To_dom.of_table table)
-            (Tyxml_js.To_dom.of_tr row) )
-        submissions ;
-      Lwt.return_unit ) ;
+      else (* code 200 *)
+        let resp_json_string = Js.to_string (Json.output resp) in
+        let submissions : Api.Openapi.submission list =
+          Api.Openapi.ContestsContestidSubmissionsGetResponse2.of_json
+            resp_json_string
+        in
+        List.iter
+          (fun (sub : Api.Openapi.submission) ->
+            let id = sub.id in
+            let status = status_of_string sub.status in
+            let time_str = format_time_ms sub.time_ms in
+            (* fetch problem title for the submission *)
+            Lwt.async (fun () ->
+                Api.Helpers.fetch_json
+                  (Printf.sprintf "%s/problems/%d" Api.Helpers.base_url
+                     sub.problem_id )
+                >>= fun (problem_resp, problem_status) ->
+                if problem_status <> 200 then (
+                  Console.console##log
+                    (Js.string
+                       (Printf.sprintf "Failed to fetch problem: %d"
+                          problem_status ) ) ;
+                  Lwt.return_unit )
+                else
+                  let p =
+                    Api.Openapi.Problem.of_json
+                      (Js.to_string (Json.output problem_resp))
+                  in
+                  (* let lang = match Js.Opt.get sub.language (fun () ->
+                     Js.string "Unknown") with | s -> Js.to_string s in*)
+                  let lang = "Unknown" in
+                  let row = submission_row id p.code lang status time_str in
+                  Dom.appendChild
+                    (Tyxml_js.To_dom.of_table table)
+                    (Tyxml_js.To_dom.of_tr row) ;
+                  Lwt.return_unit ) )
+          submissions ;
+        Lwt.return_unit ) ;
   div ~a:[a_class ["table-responsive"]] [table]
